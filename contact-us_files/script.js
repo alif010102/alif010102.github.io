76989758b3d0c00a3b9c1a27a6341172ee53c1d6
alif010102 <fajrialifantariksa.afa@gmail.com>
2025-01-27 22:04:37 +0700

    Add files via upload

--- contact-us_files/script.js
+++ contact-us_files/script.js
@@ -20,3 +20,16 @@ function showSidebar(){
     sidebar.style.display = "flex";
 }
 //Header Selesai
+function kirimWA(){
+    let namaOrang = document.getElementById('namaOrang');
+    let emailOrang = document.getElementById('emailOrang');
+    let pesan = document.getElementById('message');
+
+    let encodeNama = encodeURIComponent(namaOrang.value);
+    let encodeEmail = encodeURIComponent(emailOrang.value);
+    let encodePesan = encodeURIComponent(pesan.value);
+    let urlWA = "https://wa.me/628122072109?text="
+
+    let urlFull = urlWA + "Nama:%20%20" + encodeNama + "%0A%0A" + "Email:%20%20" + encodeEmail + "%0A%0A" + "Pesan:%20%20" + encodePesan;
+    window.open(urlFull, '_blank');
+}
--- product_files/script.js
+++ product_files/script.js
@@ -21,33 +21,41 @@ function showSidebar() {
 }
 //Header Selesai
 // Card Product
-// Nama file JSON yang berada di dalam folder 'product_files'
-const fileName = 'product_files/product.json';
+const fileName = "product_files/product.json";
+let allData = [];
 
 fetch(fileName)
     .then(response => response.json())
     .then(data => {
-        const kotakContainer = document.getElementById('main');
-
-        // Membuat string HTML untuk semua produk
-        let produkHTML = '';
-
-        data.products.forEach(product => {
-            // Menambahkan kotak untuk setiap produk ke dalam string HTML
-            produkHTML += `
-        <a onclick=(showPopup(${product.id}))>
-            <img src="product_files/images/product/${product.image}" alt="box-panel">
-            <h5>${product.title}</h5>
-            <p>${product.summary}</p>
-        </a>
-                    `;
-        });
-
-        // Mengubah konten kotak-container dengan produkHTML
-        kotakContainer.innerHTML = produkHTML;
+        allData = data; // Simpan data lengkap untuk pencarian
+        displayData(data.products);
     })
-    .catch(error => console.error('Error loading JSON:', error));
-// Card Product Selesai
+    .catch(error => console.error('Error:', error));
+
+function searchData() {
+    let searchQuery = document.getElementById("search").value.toLowerCase();
+
+    // Filter data berdasarkan query pencarian
+    let filteredData = allData.products.filter(item => item.title.toLowerCase().includes(searchQuery));
+
+    // Menampilkan data yang sudah difilter
+    displayData(filteredData);
+}
+function displayData(data) {
+    let produkHTML = '';
+    const kotakContainer = document.getElementById('main');
+    data.forEach(product => {
+        produkHTML += `
+                <a onclick=(showPopup(${product.id}))>
+                    <img src="product_files/images/product/${product.image}" alt="box-panel">
+                    <h5>${product.title}</h5>
+                    <p>${product.summary}</p>
+                </a>
+                            `;
+    });
+    kotakContainer.innerHTML = produkHTML;
+}
+// End of Card Product 
 // Popup Product
 function closePopup() {
     const popupProduct = document.querySelector('.popupProduct');
@@ -78,10 +86,10 @@ function showPopup(productId) {
     popupProduct.style.display = "flex"
 }
 // Menangani klik di luar popupContent untuk menutup popup
-window.addEventListener('click', function(event) {
+window.addEventListener('click', function (event) {
     const popup = document.querySelector('.popupProduct');
     const popupContent = document.querySelector('.popupProductContent');
-    
+
     // Jika klik di luar popupContent dan di dalam popupProduct, maka tutup popup
     if (popup.contains(event.target) && !popupContent.contains(event.target)) {
         closePopup();
